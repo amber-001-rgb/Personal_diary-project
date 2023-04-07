58133784b0c5852d55a810bca7ba2ffc237d12ba
sportynest <82081186+sportynest@users.noreply.github.com>
2023-04-06 22:48:50 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const morgan = require('morgan');
+
+// express app
+const app = express();
+
+//listen for requests
+app.listen(3000)
+
+//middleware and static files
+app.use(express.static('public'))
+app.use(morgan('dev'));
+
+//routing requests and webpages
+app.get('/signUp', (req, res) =>{
+    res.sendFile('./views/signUp.html', {root: __dirname})
+})
+
+app.get('/diary', (req, res) =>{
+    res.sendFile('./views/maindiary.html', {root: __dirname});
+})
+
+app.get('/', (req, res) =>{    
+    res.sendFile('./views/signUp.html', {root: __dirname})
+})
+
+// 404 page
+app.use((req, res) =>{
+    res.status(404).sendFile('./views/404.html', {root: __dirname})
+})
+
+//All User Info Details
+var userNames = [];
+var passWords = [];
+
+
+// can only contain diary entries for 10 accounts
+var diaryEntries = [
+    {   text_entry: [],
+    },
+    {   text_entry: [],
+    },
+    {   text_entry: [],
+    },
+    {   text_entry: [],
+    },
+    {   text_entry: [],
+    },
+    {   text_entry: [],
+    },
+    {   text_entry: [],
+    },
+    {   text_entry: [],
+    },
+    {   text_entry: [],
+    },
+    {   text_entry: [],
+    }
+];
+
+
