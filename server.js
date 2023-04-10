c090e4cf74539a7e5b0e10ee98dc8073060f01c1
sportynest <82081186+sportynest@users.noreply.github.com>
2023-04-10 17:08:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const { send } = require('process');
 
 // express app
 const app = express();
@@ -24,10 +25,61 @@ app.get('/', (req, res) =>{
     res.sendFile('./views/signUp.html', {root: __dirname})
 })
 
+app.get('/signIn', (req, res) =>{    
+    console.log("cnjds")
+    alert("nd na")
+})
+
 // 404 page
 app.use((req, res) =>{
     res.status(404).sendFile('./views/404.html', {root: __dirname})
 })
+
+//call these functions when receiving requests from the server
+function signIn(username, password){
+    usernameExists = false
+    for(i=0;i<userNames.length;i++){
+        if(username == userNames[i]){
+            usernameExists = true;
+            if(password == passWords[i]){
+                return "signedIn"
+            }else{
+                return "wrongPassword"
+            }
+        }
+    }
+    if(usernameExists == false){
+        return "noUser"
+    }
+}
+
+function signUp(username, password){
+    usernameIsTaken = false
+    for(i=0;i<userNames.length;i++){
+        if(username == userNames[i]){
+            usernameIsTaken = true
+        }
+    }
+    if(usernameIsTaken == false){
+        userNames.push(username);
+        passWords.push(password);
+        return "signedUp"
+    }
+    return "emailIsTaken"
+}
+
+function addDiaryEntry(i,entry){
+    diaryEntries[i].text_entry.push(entry);
+}
+
+function removeDiaryEntries(i,entry){
+    const index = diaryEntries[i].text_entry.indexOf(entry)
+    diaryEntries[i].text_entry.splice(index,1)
+}
+
+function returnDiaryEnties(i){
+    return diaryEntries[i].text_entry
+}
 
 //All User Info Details
 var userNames = [];
