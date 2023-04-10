08188e0e76b0355db3467d20458598dfa69cdc03
sportynest <82081186+sportynest@users.noreply.github.com>
2023-04-10 14:50:28 -0400

    Update signUp.js

--- public/signUp.js
+++ public/signUp.js
@@ -1,4 +1,51 @@
+var http = require('http');
+function signIn(){
+    document.getElementById("nameField").style.maxHeight="0";
+    document.getElementById("title").innerHTML ="Sign In"
+    document.getElementById("signupBtn").classList.add("disable");
+    document.getElementById("signupBtn").classList.remove("disable");
+    var usernameEntered = document.getElementById("emailSpace").value;
+    var passwordEntered = document.getElementById("passwordSpace").value;
+    
+    var toBeSent = JSON.stringify({
+        "usernameSent": usernameEntered,
+        "passwordSent": passwordEntered
+    })   
+    
+    /**$.post("/signIn",toBeSent, (ret)=>{
+        if(ret == true){
+            alert("ncis")
+        }else{
 
+        }
+    })**/
+    alert(usernameEntered + " " + passwordEntered)
+}
+
+function signUp(){
+    document.getElementById("nameField").style.maxHeight = "60px";
+    document.getElementById("title").innerHTML = "Sign Up"
+    document.getElementById("signupBtn").classList.add("disable");
+    document.getElementById("signupBtn").classList.remove("disable");
+    var usernameEntered = document.getElementById("emailSpace").value;
+    var passwordEntered = document.getElementById("passwordSpace").value;
+    
+    
+    var toBeSent = JSON.stringify({
+        usernameSent: usernameEntered,
+        passwordSent: passwordEntered
+    })
+
+
+    $.post("/signUp",toBeSent,function aftsignIn(ret){
+        if(ret){
+            alert("cnja")
+        }else{
+
+        }
+    })
+    alert(usernameEntered + " " + passwordEntered)
+}
 
 
 
