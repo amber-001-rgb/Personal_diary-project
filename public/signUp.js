18a4855ddc01c1a3cb8456ccef33009d0d2c5fd7
sportynest <82081186+sportynest@users.noreply.github.com>
2023-04-10 23:16:37 -0400

    Update signUp.js

--- public/signUp.js
+++ public/signUp.js
@@ -1,25 +1,35 @@
-var http = require('http');
+let signinSignout = 0;
 function signIn(){
     document.getElementById("nameField").style.maxHeight="0";
     document.getElementById("title").innerHTML ="Sign In"
     document.getElementById("signupBtn").classList.add("disable");
     document.getElementById("signupBtn").classList.remove("disable");
+    
     var usernameEntered = document.getElementById("emailSpace").value;
     var passwordEntered = document.getElementById("passwordSpace").value;
-    
-    var toBeSent = JSON.stringify({
-        "usernameSent": usernameEntered,
-        "passwordSent": passwordEntered
-    })   
-    
-    /**$.post("/signIn",toBeSent, (ret)=>{
-        if(ret == true){
-            alert("ncis")
-        }else{
+    if(signinSignout == 1){
+        var toBeSent = JSON.stringify({usernameEntered,passwordEntered});   
+        sendSignIn(toBeSent);
+    }
+   
 
-        }
-    })**/
-    alert(usernameEntered + " " + passwordEntered)
+    if (signinSignout == 0){
+        signinSignout = signinSignout + 1;
+    }
+    console.log(signinSignout)
+}
+
+function sendSignIn(toBeSent){
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: toBeSent
+    }
+    fetch('/signinServer',options).then(res=>{
+
+    })
 }
 
 function signUp(){
@@ -30,22 +40,37 @@ function signUp(){
     var usernameEntered = document.getElementById("emailSpace").value;
     var passwordEntered = document.getElementById("passwordSpace").value;
     
-    
-    var toBeSent = JSON.stringify({
-        usernameSent: usernameEntered,
-        passwordSent: passwordEntered
-    })
+    if(usernameEntered.length>7 && passwordEntered.length>7 && Name > 0){
+        if(signinSignout == 0){
+            var nameEntered = document.getElementById("nameSpace").value;
+            var toBeSent = JSON.stringify({usernameEntered,passwordEntered,nameEntered})
+            sendSignUp(toBeSent)
+        }
 
-
-    $.post("/signUp",toBeSent,function aftsignIn(ret){
-        if(ret){
-            alert("cnja")
+        if (signinSignout == 1){
+            signinSignout = signinSignout - 1;
+        }
+    }else{
+        alert("User And Password Must Contain A Minimum Of 8 Characters")
+    }
+    console.log(signinSignout)
+}
+function sendSignUp(toBeSent){  
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: toBeSent   
+    }
+    fetch('/signupServer',options).then(async res=>{
+        res = await res.json()
+        if(res == "emailIsTaken"){
+            var err = document.getElementById("emailerror")
+            err.style.visibility = "visible"
         }else{
-
+            //alert("changing")
+            //window.open('localhost:3000/diary')
         }
     })
-    alert(usernameEntered + " " + passwordEntered)
-}
-
-
-
+}  
