00748d091a3e6f953d99f785c2a8add736c536b0
sportynest <82081186+sportynest@users.noreply.github.com>
2023-04-11 17:55:22 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const morgan = require('morgan');
-const { send } = require('process');
 
 // express app
 const app = express();
@@ -9,8 +8,9 @@ const app = express();
 app.listen(3000)
 
 //middleware and static files
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.use(morgan('dev'));
+app.use(express.json({limit: '1mb'}));
 
 //routing requests and webpages
 app.get('/signUp', (req, res) =>{
@@ -21,51 +21,80 @@ app.get('/diary', (req, res) =>{
     res.sendFile('./views/maindiary.html', {root: __dirname});
 })
 
-app.get('/', (req, res) =>{    
-    res.sendFile('./views/signUp.html', {root: __dirname})
+//All User Info Details
+var names = [];
+var userNames = [];
+var passWords = [];
+
+
+app.post('/signinServer', (req, res)=> {
+    console.log("Received A Sign In Request")
+    var signin = signIn(req.body) 
+    var signin0 = signin[0]
+    var signin1 = signin[1]
+    dataEntries = returnDiaryEnties(signin1)
+    if (signIn[1] <0){
+        res.json(signin0)
+    }else{
+        res.json({signin0,dataEntries})
+    }
+})
+app.post('/signupServer', (req, res)=> {
+    console.log("Received A Sign Up Request")
+    signUpStatus = signUp(req.body)
+    console.log(signUpStatus)
+    res.json(signUpStatus);
 })
 
-app.get('/signIn', (req, res) =>{    
-    console.log("cnjds")
-    alert("nd na")
-})
-
-// 404 page
+// permanent redirect to signup page
 app.use((req, res) =>{
-    res.status(404).sendFile('./views/404.html', {root: __dirname})
+    res.redirect("/signUp")
 })
 
 //call these functions when receiving requests from the server
-function signIn(username, password){
+function signIn(req){
+    var username = '' + req.usernameEntered;
+    var password = '' + req.passwordEntered;
+    let index;
     usernameExists = false
     for(i=0;i<userNames.length;i++){
-        if(username == userNames[i]){
+        if(username.toLowerCase() === userNames[i].toLowerCase()){
+            index = i;
             usernameExists = true;
-            if(password == passWords[i]){
-                return "signedIn"
+            if(password === passWords[i]){
+                return ["signedIn",i]
             }else{
-                return "wrongPassword"
+                return ["wrongPassword", -1]
             }
         }
     }
     if(usernameExists == false){
-        return "noUser"
+        return ["noUser",-1]
     }
 }
 
-function signUp(username, password){
+function signUp(req){
+    var name = '' + req.nameEntered;
+    var username = '' + req.usernameEntered;
+    var password = '' + req.passwordEntered;
+
+    
     usernameIsTaken = false
+    index = 0;
     for(i=0;i<userNames.length;i++){
-        if(username == userNames[i]){
+        if(username.toLowerCase() === userNames[i].toLowerCase()){
             usernameIsTaken = true
+            index = i;
         }
     }
+
     if(usernameIsTaken == false){
         userNames.push(username);
         passWords.push(password);
-        return "signedUp"
+        names.push(name);
+        return ["signedUp", username , password, userNames[index], passWords[index], index]
     }
-    return "emailIsTaken"
+    return ["emailIsTaken", username , password, userNames[index], passWords[index], index]
 }
 
 function addDiaryEntry(i,entry){
@@ -78,17 +107,18 @@ function removeDiaryEntries(i,entry){
 }
 
 function returnDiaryEnties(i){
-    return diaryEntries[i].text_entry
+    return diaryEntries[i]
 }
 
 //All User Info Details
-var userNames = [];
-var passWords = [];
+var names = ["name"];
+var userNames = ["password"];
+var passWords = ["password"];
 
 
 // can only contain diary entries for 10 accounts
 var diaryEntries = [
-    {   text_entry: [],
+    {   text_entry: ["whfevwbuvew","cnno wbobewbvboevwo","ACBOVOWUBEIBIEVUWB"],
     },
     {   text_entry: [],
     },
