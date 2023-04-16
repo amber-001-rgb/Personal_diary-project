8adfc02c236a953614e905a9aa4058a4becdf202
Amber E. Moseri <107995097+amber-001-rgb@users.noreply.github.com>
2023-04-16 18:25:43 -0400

    Add files via upload

---
+++ signintest.js
@@ -0,0 +1,21 @@
+// Import necessary dependencies
+assert = chai.assert;
+
+// Define a test suite
+describe('test function', () => {
+
+  describe('test', () => {
+    it('should return true if the each userNames has a corresponding passWord are equal', () => {
+
+      const result = test(userNames, passWords);
+      assert.strictEqual(result, true);
+    });
+
+    it('should return true if each username is unique', () => {
+      const result = checkUniqueUsernames(userNames, username_length);
+      assert.strictEqual(result, true);
+    });
+  
+  
+  });
+});
